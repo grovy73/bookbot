cb06250ad6262a07d0c710f94938497f6e45848c
grovy73 <holger.bregendahl@hotmail.com>
2024-06-03 19:09:23 +0200

    Added gen_report func

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+ALLOWED_LETTERS = "abcdefghijklmnopqrstuwvxyzæøåäö"
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        char_count = count_chars(file_contents.lower())
-        print(char_count)
+    gen_report("books/frankenstein.txt")
+
+
+
+
 
 def count_words(text) -> int:
     return len(text.split(" "))
@@ -10,11 +13,23 @@ def count_words(text) -> int:
 def count_chars(text):
     res = {}
     for c in text:
+        if c not in ALLOWED_LETTERS: continue
+
         if c in res:
             res[c] += 1
         else:
             res[c] = 1
     return res;
+
+def gen_report(path):
+    with open(path) as f:
+        print("\t--- MY REPORT ---")
+        file_contents = f.read()
+        char_count = count_chars(file_contents.lower())
+        sorted_keys = sorted(char_count.keys(), key=lambda k: char_count[k], reverse=True)
+        for k in sorted_keys:
+            print(f"   The letter '{k}' was found {char_count[k]} time(s)")
+
 
 if __name__ == "__main__":
     main()
